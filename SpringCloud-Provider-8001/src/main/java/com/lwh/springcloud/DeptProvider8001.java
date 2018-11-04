381ecdd63299aaf2234cd2aeff1ec6db9217fda2
lwh <2391519868@qq.com>
2018-11-04 20:38:30 +0800

    Eureka服务发现

--- SpringCloud-Provider-8001/src/main/java/com/lwh/springcloud/DeptProvider8001.java
+++ SpringCloud-Provider-8001/src/main/java/com/lwh/springcloud/DeptProvider8001.java
@@ -2,6 +2,7 @@ package com.lwh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
@@ -9,9 +10,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author  lwh
  * @date    2018-11-04
  * @desp    @EnableEurekaClient    本服务启动后会自动注册进eureka服务中
+ *          @EnableDiscoveryClient 服务发现
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class DeptProvider8001 {
 
 	public static void main(String[] args){
--- SpringCloud-Provider-8001/src/main/java/com/lwh/springcloud/controller/DeptProvideController.java
+++ SpringCloud-Provider-8001/src/main/java/com/lwh/springcloud/controller/DeptProvideController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.lwh.springcloud.entity.Dept;
 import com.lwh.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,12 @@ public class DeptProvideController {
 	@Autowired
 	private DeptService service;
 
+	/**
+	 * 服务发现
+	 */
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
 	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept){
 		return service.add(dept);
@@ -35,6 +43,24 @@ public class DeptProvideController {
 	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
 	public List<Dept> list(){
 		return service.list();
+	}
+
+	/**
+	 * 让别人发现我这个服务,不是重点
+	 * @return
+	 */
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery(){
+		//查看Eureka里面的服务
+		List<String> services = discoveryClient.getServices();
+		System.out.println(services);
+
+		List<ServiceInstance> srvList = discoveryClient.getInstances("springcloud-dept");
+		for(ServiceInstance element : srvList){
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+		}
+
+		return this.discoveryClient;
 	}
 
 }
