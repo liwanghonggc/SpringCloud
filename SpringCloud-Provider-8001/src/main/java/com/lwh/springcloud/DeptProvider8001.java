ddb972703a406429f7e0f7ad475936a65e2d864b
lwh <2391519868@qq.com>
2018-11-04 18:39:38 +0800

    Eureka自我保护机制

--- SpringCloud-Provider-8001/src/main/java/com/lwh/springcloud/DeptProvider8001.java
+++ SpringCloud-Provider-8001/src/main/java/com/lwh/springcloud/DeptProvider8001.java
@@ -2,15 +2,16 @@ package com.lwh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 /**
  * @author  lwh
  * @date    2018-11-04
  * @desp    @EnableEurekaClient    本服务启动后会自动注册进eureka服务中
- *          @EnableDiscoveryClient 服务发现
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001 {
 
 	public static void main(String[] args){
